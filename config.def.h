aa301ccff5564dc6ae5a434b97ce421dc36a60a2
Folkert Bleichrodt <fbleichrodt@gmail.com>
2020-12-18 21:40:27 +0100

    Local

--- config.def.h
+++ config.def.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx     = 0;  // 6;        /* gaps between windows */
+static const unsigned int borderpx  = 0;//3;        /* border pixel of windows */
+static const unsigned int gappx     = 8;  // 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
